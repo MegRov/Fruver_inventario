892cce9d1b5729de119ca9c61b18df1343779f89
Miguel Guevara <lxmxg.96@outlook.com>
2020-04-16 13:12:25 -0500

    first commit

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: fruver-INV
+| Version: v2
+| Offcial page: http://fruverpp.com/
+| facebook Page: https://www.facebook.com/fruverpp
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'fruver_inv' );        // Set database name
+
+?>
